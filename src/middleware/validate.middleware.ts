0f08e589a3c3a2538ffbb82d3270a890d96cce02
Sergei Borja <sergei.borja0701@gmail.com>
2026-04-05 12:58:55 -0600

    Add `validate` middleware for schema validation using Zod

---
+++ src/middleware/validate.middleware.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from 'express';
+import { ZodObject } from 'zod';
+
+type Schema = {
+    body: ZodObject;
+    params: ZodObject;
+    query: ZodObject;
+}
+
+export const validate = (schema: Schema) => {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        if (schema.body)
+            req.body = await schema.body.parseAsync(req.body);
+        if (schema.params)
+            req.params = await schema.params.parseAsync(req.params) as any;
+        if (schema.query)
+            req.query = await schema.query.parseAsync(req.query) as any;
+
+        next();
+    };
+};
